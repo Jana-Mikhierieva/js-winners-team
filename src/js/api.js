2fb4ccddd02d323a950d7688b278973aab65c6d8
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-09-07 01:46:08 +0200

    reviews/added functions,style and markup for work with swiper

--- src/js/api.js
+++ src/js/api.js
@@ -4,11 +4,14 @@ import iziToast from "izitoast";
 axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api/reviews';
 
 export async function getReview() {
+	const errorText = document.querySelector('.js-text-error');
+	const btnContainer = document.querySelector('.js-reviews-btn');
     try {
 	    const response = await axios.get();
 	    
-	  if (response.data.length === 0) {
-  
+	    if (response.data.length === 0) {
+		  btnContainer.classList.add('reviews__error');
+		  errorText.classList.remove('reviews__error');
 		  iziToast.error({
 			message: "Sorry, no Reviews found.",
 			position: 'bottomRight',
@@ -17,7 +20,9 @@ export async function getReview() {
 	    }
 	    return response.data;
     } catch {
-        iziToast.error({
+	    btnContainer.classList.add('reviews__error');
+	    errorText.classList.remove('reviews__error');
+          iziToast.error({
 			message: "Sorry, no Reviews found.",
 			position: 'bottomRight',
 		  });
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,23 +1,79 @@
-import { getReview } from "./api"; 
+import { getReview } from "./api";
+import Swiper from 'swiper/bundle'; 
 
 const reviewsContainer = document.querySelector('.js-reviews');
-const reviews = await getReview();
 
-function createMarkupReviews (array) {
-	return array.map(ar => `
-	  <li class="reviews__item">
-		<img 
-		  class="reviews__image" 
-		  src="${ar.avatar_url}" 
-		  alt="commentator's photo" 
-		/>
-		<h4 class="reviews__title">"${ar.author}"</h4>
-		<p> class="reviews__text">"${ar.review}"</p>
-	  </li>`).join('');
-}
+window.onload = async () => {
+    const reviews = await getReview();
 
-function renderMarkupReviews(callback,array) {
-	reviewsContainer.insertAdjacentHTML('beforeend', callback(array));
-}
+    function createMarkupReviews(array) {
+        return array.map(ar => `
+          <li class="reviews__item swiper-slide">
+            <img 
+              class="reviews__image" 
+              src="${ar.avatar_url}" 
+              alt="commentator's photo" 
+            />
+            <h4 class="reviews__title">${ar.author}</h4>
+            <p class="reviews__text">${ar.review}</p>
+          </li>`).join('');
+    }
 
-renderMarkupReviews(createMarkupReviews, reviews);
+    function renderMarkupReviews(callback, array) {
+	  reviewsContainer.insertAdjacentHTML('beforeend', callback(array));
+    }
+
+    renderMarkupReviews(createMarkupReviews, reviews);
+
+    const swiper = new Swiper('.swiper', {
+        keyboard: {
+            enabled: true,
+            onlyInViewport: false,
+        },
+        navigation: {
+            nextEl: '.reviews__btn--next',
+            prevEl: '.reviews__btn--prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1440: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+        },
+    });
+	
+	swiper.init();
+	
+    function disabledNavigationButtons() {
+        const prevButton = document.querySelector('.js-swiper-prev');
+        const nextButton = document.querySelector('.js-swiper-next');
+
+        if (swiper.isBeginning) {
+            prevButton.classList.add('reviews__disabled');
+        } else {
+            prevButton.classList.remove('reviews__disabled');
+        }
+
+        if (swiper.isEnd) {
+            nextButton.classList.add('reviews__disabled');
+        } else {
+            nextButton.classList.remove('reviews__disabled');
+        }
+	}
+	
+    disabledNavigationButtons();
+
+    swiper.on('slideChange', disabledNavigationButtons);
+};
