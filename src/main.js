3a8e19eb0182803c53b16fdae6f1a4ac06600182
Yana Mikhierieva <mikhereva@gmail.com>
2024-09-06 14:15:46 +0200

    added img, updated main.js, css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
 import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
 import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
+
