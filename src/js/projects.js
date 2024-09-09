f5f80c2ef9a91db3912edc8b66b3914e5eb842e1
Vitalii Artiukh <radial_car@yahoo.com>
2024-09-09 19:46:44 +0300

    swiper-fixed

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,83 +1,91 @@
 // import Swiper from 'swiper/bundle';
 import Swiper from 'swiper';
+// import { disabledNavigationButtons } from './disableNav';
 import 'swiper/css';
 import LazyLoad from 'vanilla-lazyload';
 
+// const refsOption = {
+//   prevButton: document.querySelector('.left-button'),
+//   nextButton: document.querySelector('.right-button'),
+//   prevIcon: document.querySelector('.left-icon'),
+//   nextIcon: document.querySelector('.right-icon'),
+// };
+
+// const classOptions = {
+//   disabledBtnClass: 'button-disabled-projects',
+//   disabledIconClass: 'icon-disabled',
+//   disabledAttribute: 'disabled',
+// };
+
+// const swiperProg = new Swiper('.projects-swiper', {
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: false,
+//   },
+//   navigation: {
+//     nextEl: '.right-button',
+//     prevEl: '.left-button',
+//   },
+//   on: {
+//     init: function () {
+//       disabledNavigationButtons(this, refsOption, classOptions);
+//     },
+//     slideChange: function () {
+//       disabledNavigationButtons(this, refsOption, classOptions);
+//     },
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 16,
+//     },
+//     375: {
+//       slidesPerView: 1,
+//       spaceBetween: 16,
+//     },
+//     768: {
+//       slidesPerView: 1,
+//       spaceBetween: 16,
+//     },
+//     1440: {
+//       slidesPerView: 1,
+//       spaceBetween: 16,
+//     },
+//   },
+// });
+
 const nextButton = document.querySelector('.right-button');
 
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  spaceBetween: 16,
+const mySwiper = new Swiper('.projects-swiper', {
   speed: 600,
-  breakpointsBase: 'container',
-  centeredSlides: true,
-  maxBackfaceHiddenSlides: 3,
-
   keyboard: {
     enabled: true,
     onlyInViewport: true,
   },
-
   navigation: {
     nextEl: '.right-button',
     prevEl: '.left-button',
   },
 
-  // on: {
-  //   init: function () {
-  //     disabledNavigationButtonsVit(this, refsOption, classOptions);
-  //   },
-  //   slideChange: function () {
-  //     disabledNavigationButtonsVit(this, refsOption, classOptions);
-  //   },
-  // },
-
-  // breakpoints: {
-  //   320: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 16,
-  //   },
-  //   375: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 16,
-  //   },
-  //   768: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 16,
-  //   },
-  //   1440: {
-  //     slidesPerView: 1,
-  //     spaceBetween: 16,
-  //   },
-  // },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    1440: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+  },
 });
 
-// function disabledNavigationButtonsVit(el, refsOption, classOptions) {
-//   const { prevButton, nextButton, prevIcon, nextIcon } = refsOption;
-//   const { disabledBtnClass, disabledIconClass, disabledAttribute } =
-//     classOptions;
-
-//   const toggleClass = (element, condition, className) => {
-//     condition
-//       ? element.classList.add(className)
-//       : element.classList.remove(className);
-//   };
-
-//   const toggleAttribute = (element, condition, attrName) => {
-//     condition
-//       ? element.setAttribute(attrName, 'true')
-//       : element.removeAttribute(attrName);
-//   };
-
-//   toggleClass(prevButton, el.isBeginning, disabledBtnClass);
-//   toggleClass(prevIcon, el.isBeginning, disabledIconClass);
-//   toggleAttribute(prevButton, el.isBeginning, disabledAttribute);
-
-//   toggleClass(nextButton, el.isEnd, disabledBtnClass);
-//   toggleClass(nextIcon, el.isEnd, disabledIconClass);
-//   toggleAttribute(nextButton, el.isEnd, disabledAttribute);
-// }
-
-nextButton.disabled = false;
-nextButton.classList.remove('swiper-button-disabled');
-// refsOption.nextIcon.classList.remove('icon-disabled');
+// refsOption.nextButton.disabled = false;
+// refsOption.nextButton.classList.remove('swiper-button-disabled');
