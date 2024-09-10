e0676486ded42fcb62d9ec5e777b3cd1d984efc0
Yana Mikhierieva <mikhereva@gmail.com>
2024-09-10 18:39:59 +0200

    updatet css about, covers, js rewiews, partials about

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,13 +1,13 @@
-import { getReview } from "./api";
+import { getReview } from './api';
 import Swiper from 'swiper/bundle';
-import LazyLoad from "vanilla-lazyload";
+import LazyLoad from 'vanilla-lazyload';
 import $ from 'jquery';
 import 'magnific-popup';
 import { disabledNavigationButtons } from './disableNav';
 
 // Ініціалізація бібліотеки vanilla-lazyload
 const loadingDeferredInstance = new LazyLoad({
-    elements_selector: ".lazy__load"
+  elements_selector: '.lazy__load',
 });
 
 // Отримання посилання на контейнер у якому відрендериться відповідь з бекенду
@@ -15,83 +15,85 @@ const reviewsContainer = document.querySelector('.js-reviews');
 
 // Об'єкт налаштувань з посиланнями на елементи які треба відслідковувати для зміни класу для функції disabledNavigationButtons
 const refsOption = {
-    prevButton: document.querySelector('.reviews__btn--prev'),
-    nextButton: document.querySelector('.reviews__btn--next'),
-    prevIcon: document.querySelector('.js-review-icon-prev'),
-    nextIcon: document.querySelector('.js-review-icon-next'),
+  prevButton: document.querySelector('.reviews__btn--prev'),
+  nextButton: document.querySelector('.reviews__btn--next'),
+  prevIcon: document.querySelector('.js-review-icon-prev'),
+  nextIcon: document.querySelector('.js-review-icon-next'),
 };
 
 // Об'єкт налаштувань з класами які треба змінювати на елементах у функції disabledNavigationButtons
 const classOptions = {
-    disabledBtnClass: 'reviews__btn--disabled',
-    disabledIconClass: 'reviews__icon--disabled',
-    disabledAttribute: 'disabled',
+  disabledBtnClass: 'reviews__btn--disabled',
+  disabledIconClass: 'reviews__icon--disabled',
+  disabledAttribute: 'disabled',
 };
 
 // Функція ініціалізації
 function initializeSwiper() {
-    // Ініціалізіція Swiper
-    const swiperRev = new Swiper('.reviews-swiper', { 
-        keyboard: {
-            enabled: true,
-            onlyInViewport: false,
-        },
-        navigation: {
-            nextEl: '.reviews__btn--next',
-            prevEl: '.reviews__btn--prev',
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 16,
-            },
-            375: {
-                slidesPerView: 1,
-                spaceBetween: 16,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 16,
-            },
-            1440: {
-                slidesPerView: 4,
-                spaceBetween: 16,
-            },
-        },
-        on: {
-            init: function () {
-                disabledNavigationButtons(this, refsOption, classOptions);
-            },
-            slideChange: function () {
-                disabledNavigationButtons(this, refsOption, classOptions);
-            },
-            reachBeginning: function () {
-                disabledNavigationButtons(this, refsOption, classOptions);
-            },
-            reachEnd: function () {
-                disabledNavigationButtons(this, refsOption, classOptions);
-            },
-        },
-    });
+  // Ініціалізіція Swiper
+  const swiperRev = new Swiper('.reviews-swiper', {
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    navigation: {
+      nextEl: '.reviews__btn--next',
+      prevEl: '.reviews__btn--prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      375: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      1440: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+    },
+    on: {
+      init: function () {
+        disabledNavigationButtons(this, refsOption, classOptions);
+      },
+      slideChange: function () {
+        disabledNavigationButtons(this, refsOption, classOptions);
+      },
+      reachBeginning: function () {
+        disabledNavigationButtons(this, refsOption, classOptions);
+      },
+      reachEnd: function () {
+        disabledNavigationButtons(this, refsOption, classOptions);
+      },
+    },
+  });
 
-    // Ініціалізація Magnific Popup
-    $('.open-popup-link').magnificPopup({
-        type: 'inline',
-        midClick: true
-    });
+  // Ініціалізація Magnific Popup
+  $('.open-popup-link').magnificPopup({
+    type: 'inline',
+    midClick: true,
+  });
 }
 
 // Запуск коду після завантаження сторінки у методі який відпрацює за завантаження після того як будуть завантажені стилі,html і т.д
 window.onload = async () => {
-    const reviews = await getReview();
-    createMarkupReviews(renderMarkupReviews, reviews);
-    initializeSwiper();
-    loadingDeferredInstance.update();
+  const reviews = await getReview();
+  createMarkupReviews(renderMarkupReviews, reviews);
+  initializeSwiper();
+  loadingDeferredInstance.update();
 };
 
 // Функція для рендерингу розмітки відгуків
 function renderMarkupReviews(array) {
-    return array.map((ar, index) => `
+  return array
+    .map(
+      (ar, index) => `
         <li class="reviews__item swiper-slide">
             <a href="#review${index}" class="open-popup-link">
                 <img 
@@ -99,7 +101,7 @@ function renderMarkupReviews(array) {
                     data-src="${ar.avatar_url}" 
                     alt="commentator's photo" 
                 />
-                <h4 class="reviews__title">${ar.author}</h4>
+                <h3 class="reviews__title">${ar.author}</h3>
                 <p class="reviews__text">${ar.review}</p>
             </a>
             <div id="review${index}" class="mfp-hide">
@@ -107,15 +109,17 @@ function renderMarkupReviews(array) {
                     <button class="mfp-close">&times;</button>
                     <img data-src="${ar.avatar_url}" alt="${ar.author}" class="popup-image lazy__load" />
                     <div class="popup-content">
-                        <h2>${ar.author}</h2>
+                        <h3>${ar.author}</h3>
                         <p>${ar.review}</p>
                     </div>
                 </div>
             </div>
-        </li>`).join('');
+        </li>`
+    )
+    .join('');
 }
 
 // Функція для створення розмітки відгуків
 function createMarkupReviews(callback, array) {
-    reviewsContainer.insertAdjacentHTML('beforeend', callback(array));
+  reviewsContainer.insertAdjacentHTML('beforeend', callback(array));
 }
